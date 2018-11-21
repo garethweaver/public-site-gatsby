19b678965f5aab2737ca1dde73e39fed30e21865
garethweaver <reg@garethweaver.com>
2018-11-21 17:49:36 +0000

    Add support for desktop and mobile webp images

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,28 @@
 import {createStore} from 'redux'
 
-const state = {
-  modal: false,
-  mobileMenu: false
+let canWebP = () => {
+  return typeof document !== 'undefined' &&
+    document
+      .createElement('canvas')
+      .toDataURL('image/webp')
+      .indexOf('data:image/webp') === 0
 }
+
+let handleResize = (e) => {
+  let isMobile = typeof window !== 'undefined' && window.innerWidth < 768
+  if (store.getState().isMobile !== isMobile) {
+    store.dispatch({type: 'SET_IS_MOBILE', data: isMobile});
+  }
+}
+
+const defaultState = {
+  modal: false,
+  mobileMenu: false,
+  webp: false,
+  isMobile: true
+}
+
+defaultState.webp = canWebP()
 
 let rootReducer = (state = {}, action) => {
   switch (action.type) {
@@ -33,7 +52,12 @@ let rootReducer = (state = {}, action) => {
   }
 }
 
-const store = createStore(rootReducer, state)
+const store = createStore(rootReducer, defaultState)
+
+if (typeof window !== 'undefined') {
+  window.addEventListener('resize', handleResize)
+  handleResize()
+}
 
 // store.subscribe(() => {
 //   console.log(store.getState())
