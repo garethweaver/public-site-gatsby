d2992e3454d00686e8026ca745de3059a93edc4a
garethweaver <reg@garethweaver.com>
2020-01-31 15:52:32 +0000

    refactor(gatsby-config.js): single quotes to backticks

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,29 @@
-require('dotenv').config()
+require(`dotenv`).config()
 
 module.exports = {
   siteMetadata: {
-    title: 'Gareth Weaver - Front-end Web Designer & Developer',
-    description: 'I\'m a front-end web designer and developer. I\'m always happy to have a chat about websites, hosting, technology or rugby. Why not send me a message?',
+    title: `Gareth Weaver - Front-end Web Designer & Developer`,
+    description: `I'm a front-end web designer and developer. I'm always happy to have a chat about websites, hosting, technology or rugby. Why not send me a message?`,
     url: process.env.PUBLIC_URL,
-    image: '/images/img-og.jpg',
+    image: `/images/img-og.jpg`,
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Gareth Weaver',
-        short_name: 'GW',
-        start_url: '/',
-        background_color: '#ffffff',
-        theme_color: '#ffffff',
-        display: 'minimal-ui',
-        icon: 'src/images/site-icon.png', // This path is relative to the root of the site.
+        name: `Gareth Weaver`,
+        short_name: `GW`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
+        display: `minimal-ui`,
+        icon: `src/images/site-icon.png`, // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-offline',
-    'gatsby-plugin-sass',
-    'gatsby-transformer-remark',
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-alias-imports`,
       options: {
@@ -35,9 +35,9 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
       options: {
-        path: './src/data/',
+        path: `./src/data/`,
       },
     },
     {
@@ -57,7 +57,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-22525855-2',
+        trackingId: `UA-22525855-2`,
         head: true,
         sampleRate: 5,
         siteSpeedSampleRate: 10,
